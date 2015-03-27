d96af97e329a4f31a98fca95c95f806f3b4fd238
hendrik <hchoy21@gmail.com>
2015-03-26 21:32:34 -0400

    added 2 stubs, CalculatorStub and BubbleSortStub. Tester class to utilize annotations

--- src/CustomTestRunner.java
+++ src/CustomTestRunner.java
@@ -1,8 +1,20 @@
+import java.lang.reflect.Method;
+
 
 public class CustomTestRunner {
 	
-	@IgnorePassed
-	public void testThisThingie(){
+	public static void main(String[] args) throws Exception{
+		
+		
+		int passed = 0, failed = 0;
+		
+		Object obj = Class.forName(args[0]).newInstance();
+		
+		for(Method m: Class.forName(args[0]).getMethods()){
+			
+		}
+		
 		
 	}
+
 }

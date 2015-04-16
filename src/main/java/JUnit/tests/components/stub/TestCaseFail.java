0ce4982ffd136f417eb88605315eddc07ab9e3b3
n-chunky <huywynn283@gmail.com>
2015-04-16 01:05:27 -0400

    implemented CPULimitTest and AmpleMemory

--- src/main/java/JUnit/tests/components/stub/TestCaseFail.java
+++ src/main/java/JUnit/tests/components/stub/TestCaseFail.java
@@ -1,6 +1,7 @@
 package JUnit.tests.components.stub;
 
 import JUnit.tests.components.AmpleMemory;
+import JUnit.tests.components.CPULimitTest;
 import JUnit.tests.components.ExpectedCalls;
 import JUnit.tests.components.IgnorePassed;
 import JUnit.tests.components.Randomize;
@@ -23,13 +24,11 @@ public class TestCaseFail {
 		System.out.println("this should print 2 times");
 	}
 	
-	@AmpleMemory
-	public void testOutOfMemory() throws InterruptedException{
-		int v = 20;
-		while(1==1){
-			int[] memoryFill = new int[v];
-			v = v * 20;
-			Thread.sleep(50);
+	@AmpleMemory (threshHold=100)
+	@CPULimitTest (limit=1)
+	public void testRandomNumberFail(){
+		for(int i=0;i<1000;i++){
+			Object[] a = new Object[2000];
 		}
 	}
 	
